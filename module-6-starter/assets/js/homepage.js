f512dd23f970d40260afb56396407c4542cf0b3b
adrian <sargentamp149@gmail.com>
2020-11-18 13:31:58 -0500

    progress

--- module-6-starter/assets/js/homepage.js
+++ module-6-starter/assets/js/homepage.js
@@ -9,5 +9,22 @@ var getUserRepos = function(user) {
     });
   });
 };
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
   
-  getUserRepos()
+var formSubmitHandler = function(event) {
+  event.preventDefault();
+  // get value from input element
+var username = nameInputEl.value.trim();
+
+if (username) {
+  getUserRepos(username);
+  nameInputEl.value = "";
+} else {
+  alert("Please enter a GitHub username");
+}
+  console.log(event);
+};
+
+
+userFormEl.addEventListener("submit", formSubmitHandler);
